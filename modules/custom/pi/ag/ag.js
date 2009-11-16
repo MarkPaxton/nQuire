296c7f4231fd0b2cc4374e56d30a3bda6970e3d3
Paul Mulholland <p.mulholland@open.ac.uk>
2009-11-16 13:54:23 +0000

    Data flow to choose correct measures from multiple inquiries

--- modules/custom/pi/ag/ag.js
+++ modules/custom/pi/ag/ag.js
@@ -14,43 +14,48 @@ var pulloutopen = 0;
 $(document).ready(function() {
 
   // toggles the slickbox on click the noted link
-  $('#ag_guide a#inquiry-toggle').click(function() {
-    $('#ag_guide  #ag_pullout').toggle(400);
+  $('a#inquiry-toggle').click(function() {
+    $('#ag_pullout').toggle(400);
     if (pulloutopen == 1) {
-      $('#ag_guide img#ag_handle').attr({src:"images/unit-navigator-open.png"});
+      $('img#ag_handle').attr({src:"images/unit-navigator-open.png"});
       pulloutopen = 0;
     }
     else {
-      $('#ag_guide img#ag_handle').attr({src:"images/unit-navigator-close.png"});
+      $('img#ag_handle').attr({src:"images/unit-navigator-close.png"});
       pulloutopen = 1;
     };
     return false;
   });
   
-  // tabs reset on mouseout
-  $('#ag_guide div.ag_inquiry').mouseout(function(){
-    reset_stages();
+  // tabs show on mouseover
+  $(function() {
+  	$(".tabs").tabs({
+  		event: 'mouseover'
+  	});
   });
-    
-  $('#ag_guide a#ag_prev').click(function (){
+  
+  // tabs reset on mouseout
+  $('div.ag_inquiry').mouseout(
+    function(){
+      set_stages();
+    }
+  );
+  
+  $('a#ag_prev').click(function (){
     inquiry_rotate_up();
   });
   
-  $('#ag_guide a#ag_next').click(function (){
+  $('a#ag_next').click(function (){
     inquiry_rotate_down();
   });
   
-  // tabs show on mouseover
-	$('#ag_guide .tabs').tabs({
-		event: 'mouseover'
-	});
-    
+  
   function inquiry_rotate_up() {
     prev_inquiry = current_inquiry;
     next_inquiry = (current_inquiry + 1) % inquiry_count;
-    $("#ag_guide div.ag_inquiry:eq(" + next_inquiry + ")").css('top','80px');
-    $("#ag_guide div.ag_inquiry:eq(" + current_inquiry + ")").animate({top: -70},"slow");
-    $("#ag_guide div.ag_inquiry:eq(" + next_inquiry + ")").show().animate({top: 5},"slow");
+    $("div.ag_inquiry:eq(" + next_inquiry + ")").css('top','80px')
+    $("div.ag_inquiry:eq(" + current_inquiry + ")").animate({top: -70},"slow");
+    $("div.ag_inquiry:eq(" + next_inquiry + ")").show().animate({top: 5},"slow");
     current_inquiry = next_inquiry;
   };
   
@@ -61,18 +66,18 @@ $(document).ready(function() {
       prev_inquiry = prev_inquiry % inquiry_count;
     };
     next_inquiry = current_inquiry;
-    $("#ag_guide div.ag_inquiry:eq(" + prev_inquiry + ")").css('top','-70px');
-    $("#ag_guide div.ag_inquiry:eq(" + current_inquiry + ")").animate({top: 80},"slow");
-    $("#ag_guide div.ag_inquiry:eq(" + prev_inquiry + ")").show().animate({top: 5},"slow");
+    $("div.ag_inquiry:eq(" + prev_inquiry + ")").css('top','-70px')
+    $("div.ag_inquiry:eq(" + current_inquiry + ")").animate({top: 80},"slow");
+    $("div.ag_inquiry:eq(" + prev_inquiry + ")").show().animate({top: 5},"slow");
     current_inquiry = prev_inquiry;
   };
   
-  function reset_stages() {
+  function set_stages() {
     var j = 0;
-    $("#ag_guide div.ag_inquiry").each(
+    $("div.ag_inquiry").each(
       function(){
-        current_stage = $("#ag_guide div.ag_inquiry:eq("+j+")").attr("stage");
-        $("#ag_guide div.ag_inquiry:eq("+j+")").find(".tabs").tabs('select', current_stage);
+        current_stage = $("div.ag_inquiry:eq("+j+")").attr("stage");
+        $("div.ag_inquiry:eq("+j+")").find(".tabs").tabs('select', current_stage);
         j = j + 1;
       }
     );
@@ -80,14 +85,14 @@ $(document).ready(function() {
   
   // hides the slickbox as soon as the DOM is ready
   // (a little sooner than page load)
-  $('#ag_guide #ag_pullout').hide();
+  $('#ag_pullout').hide();
   
   // set tabs to select currentstage tab
-  reset_stages();
+  set_stages();
   
   // calculate number of inquiries and put current inquiry at the top
-  inquiry_count = $("#ag_guide div.ag_inquiry").size();
-  current_inquiry = $("#ag_guide #ag_currentinquiry").text() - 1;
-  $("#ag_guide div.ag_inquiry:eq("+current_inquiry+")").css('top','5px');
-  
+  inquiry_count = $("div.ag_inquiry").size();
+  current_inquiry = $("#ag_currentinquiry").text() - 1;
+  $("div.ag_inquiry:eq("+current_inquiry+")").css('top','5px');
+
 });
