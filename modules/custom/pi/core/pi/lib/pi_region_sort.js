3175d609067d044ac55d87c16f2678392229307b
Mark Paxton <mcp@cs.nott.ac.uk>
2010-07-28 18:31:00 +0100

    Added global group editing admin page, with code to fix duplicate and conflicting group definitions. Also includes a new template for region-sort forms in pi module

---
+++ modules/custom/pi/core/pi/lib/pi_region_sort.js
@@ -0,0 +1,99 @@
+// $Id: block.js,v 1.2 2010/07/26 10:36:53 mcp  
+
+/**
+ * Move a block in the blocks table from one region to another via select list.
+ *
+ * This behavior is dependent on the tableDrag behavior, since it uses the
+ * objects initialized in that behavior to update the row.
+ */
+Drupal.behaviors.blockDrag = function(context) {
+  var table = $('table#sort_list');
+  var tableDrag = Drupal.tableDrag.sort_list; // Get the blocks tableDrag object.
+
+  // Add a handler for when a row is swapped, update empty regions.
+  tableDrag.row.prototype.onSwap = function(swappedRow) {
+    checkEmptyRegions(table, this);
+  };
+
+  // A custom message for the blocks page specifically.
+  Drupal.theme.tableDragChangedWarning = function () {
+    return '<div class="warning">' + Drupal.theme('tableDragChangedMarker') + ' ' + Drupal.t("Changes will not be saved until the <em>Save</em> button is clicked.") + '</div>';
+  };
+
+  // Add a handler so when a row is dropped, update fields dropped into new regions.
+  tableDrag.onDrop = function() {
+    dragObject = this;
+    
+    //alert($(dragObject.rowObject.element).prev('tr'));
+    
+    if ($(dragObject.rowObject.element).prev('tr').is('.region-message')) { 
+      var regionRow = $(dragObject.rowObject.element).prev('tr').get(0);
+      var dragElement = dragObject.rowObject.element;
+      var regionName = regionRow.className.replace(/([^ ]+[ ]+)*region-([^ ]+)-message([ ]+[^ ]+)*/, '$2');       
+      var regionField = $('select.sort_list-region-select', dragObject.rowObject.element);
+      var weightField = $('select.sort_list-weight', dragObject.rowObject.element);
+      var oldRegionName = weightField[0].className.replace(/([^ ]+[ ]+)*sort_list-weight-([^ ]+)([ ]+[^ ]+)*/, '$2');
+
+      if (!regionField.is('.sort_list-region-'+ regionName)) {
+        regionField.removeClass('sort_list-region-' + oldRegionName).addClass('sort_list-region-' + regionName);
+        weightField.removeClass('sort_list-weight-' + oldRegionName).addClass('sort_list-weight-' + regionName);
+        regionField.val(regionName);
+      }
+    }
+  };
+
+  // Add the behavior to each region select list.
+  $('select.sort_list-region-select:not(.blockregionselect-processed)', context).each(function() {
+    $(this).change(function(event) {
+      // Make our new row and select field.
+      var row = $(this).parents('tr:first');
+      var select = $(this);
+      tableDrag.rowObject = new tableDrag.row(row);
+
+      // Find the correct region and insert the row as the first in the region.
+      $('tr.region-message', table).each(function() {
+        if ($(this).is('.region-' + select[0].value + '-message')) {
+          // Add the new row and remove the old one.
+          $(this).after(row);
+          // Manually update weights and restripe.
+          tableDrag.updateFields(row.get(0));
+          tableDrag.rowObject.changed = true;
+          if (tableDrag.oldRowElement) {
+            $(tableDrag.oldRowElement).removeClass('drag-previous');
+          }
+          tableDrag.oldRowElement = row.get(0);
+          tableDrag.restripeTable();
+          tableDrag.rowObject.markChanged();
+          tableDrag.oldRowElement = row;
+          $(row).addClass('drag-previous');
+        }
+      });
+
+      // Modify empty regions with added or removed fields.
+      checkEmptyRegions(table, row);
+      // Remove focus from selectbox.
+      select.get(0).blur();
+    });
+    $(this).addClass('blockregionselect-processed');
+  });
+
+  var checkEmptyRegions = function(table, rowObject) {
+    $('tr.region-message', table).each(function() {
+      // If the dragged row is in this region, but above the message row, swap it down one space.
+      if ($(this).prev('tr').get(0) == rowObject.element) {
+        // Prevent a recursion problem when using the keyboard to move rows up.
+        if ((rowObject.method != 'keyboard' || rowObject.direction == 'down')) {
+          rowObject.swap('after', this);
+        }
+      }
+      // This region has become empty
+      if ($(this).next('tr').is(':not(.draggable)') || $(this).next('tr').size() == 0) {
+        $(this).removeClass('region-populated').addClass('region-empty');
+      }
+      // This region has become populated.
+      else if ($(this).is('.region-empty')) {
+        $(this).removeClass('region-empty').addClass('region-populated');
+      }
+    });
+  };
+};
