5f3af1cf24b670209db837341c9f4feb06d6d479
evilfer <evilfer@gmail.com>
2013-02-21 16:20:16 +0000

    - Google map measure working.

--- modules/custom/pi/common/nquire_commons/js/DynamicMeasureService.js
+++ modules/custom/pi/common/nquire_commons/js/DynamicMeasureService.js
@@ -14,6 +14,13 @@ $(function() {
   DynamicMeasureServiceDelegate.prototype.getData = function() {
     return this._service._getData(this._elementId);
   };
+  
+  DynamicMeasureServiceDelegate.prototype.startRandomDelayProcess = function() {
+  };
+
+  DynamicMeasureServiceDelegate.prototype.stopRandomDelayProcess = function() {
+  };
+
 
   nQuireJsSupport.register('DynamicMeasureService', {
     _measureHandlers: {},
@@ -25,11 +32,49 @@ $(function() {
       handler.initMeasureValue(this._getData(elementId));
     },
     _saveData: function(elementId, data) {
-
+      $('input[name="' + elementId + '"]').val(data);
     },
     _getData: function(elementId) {
-      return $('input#' + elementId).val();
+      return $('input[name="' + elementId + '"]').val();
     }
   });
 });
+
+
+(function($) {
+  var methods = {
+    init: function(options) {
+      this.addClass('dynamic_measure_link');
+      if (options.disabled) {
+        this.addClass('dynamic_measure_link_disabled');
+      }
+
+      this.click(function() {
+        if (!$(this).hasClass('dynamic_measure_link_disabled')) {
+          options.callback();
+        }
+        return false;
+      });
+      return this;
+    },
+    enable: function() {
+      this.removeClass('dynamic_measure_link_disabled');
+    },
+    disable: function() {
+      this.addClass('dynamic_measure_link_disabled');
+    }
+  };
+
+  $.fn.nQuireDynamicMeasureLink = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      console.log('Method ' + method + ' does not exist on jQuery.nQuireDynamicMeasureLink');
+      return false;
+    }
+  };
+})(jQuery);
+
 
--- modules/custom/pi/plugins/google_maps_measure/js/GoogleMapMeasure.js
+++ modules/custom/pi/plugins/google_maps_measure/js/GoogleMapMeasure.js
@@ -4,11 +4,32 @@
 $(function() {
 
   var GoogleMapMeasureManager = function(element, measureService) {
+    var self = this;
     this._measureService = measureService;
     this._element = $(element);
     this._mapElement = this._element.find('div.map_canvas');
+
+    this._map = null;
+    this._marker = null;
+    this._elevator = null;
+
     this._markButton = this._element.find('a.add_marker');
+    this._markButton.nQuireDynamicMeasureLink({
+      callback: function() {
+        self.setMarker();
+        self.updateData();
+      },
+      disabled: true
+    });
     this._forgetButton = this._element.find('a.forget_marker');
+    this._forgetButton.nQuireDynamicMeasureLink({
+      callback: function() {
+        self.forgetMarker();
+        self.updateData();
+      },
+      disabled: true
+    });
+
     this._ready = false;
   };
 
@@ -17,15 +38,128 @@ $(function() {
   };
 
   GoogleMapMeasureManager.prototype.initMeasureValue = function(value) {
+    var self = this;
     console.log('value: ' + value);
 
-    var myCenter = new google.maps.LatLng(60.0, 105.0);
+    var data = null;
+    var markerSet = false;
+    if (value && value.length > 0) {
+      try {
+        data = JSON.parse(value);
+        markerSet = true;
+      } catch (e) {
+        data = null;
+      }
+    }
+
+    if (!data && $.cookie(self._element.attr('id'))) {
+      data = JSON.parse($.cookie(self._element.attr('id')));
+    }
+
+    if (!data || typeof(data.latitude) === 'undefined' || typeof(data.longitude) === 'undefined' || typeof(data.zoom) === 'undefined') {
+      data = {
+        'latitude': 54.67500344265723,
+        'longitude': -3.4570312499999734,
+        'zoom': 4
+      };
+    }
+
+
+    var myCenter = new google.maps.LatLng(data.latitude, data.longitude);
     var myOptions = {
-      zoom: 6,
+      zoom: data.zoom,
       center: myCenter,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     this._map = new google.maps.Map(this._mapElement[0], myOptions);
+    if (markerSet) {
+      this.setMarker(data);
+    }
+
+    this._elevator = new google.maps.ElevationService();
+
+
+    google.maps.event.addListener(this._map, 'bounds_changed', function() {
+      var center = self._map.getCenter();
+      var pos = {
+        'latitude': center.lat(),
+        'longitude': center.lng(),
+        'zoom': self._map.getZoom()
+      };
+
+      var posStr = JSON.stringify(pos);
+      $.cookie(self._element.attr('id'), posStr);
+    });
+
+    this._markButton.nQuireDynamicMeasureLink('enable');
+  };
+
+  GoogleMapMeasureManager.prototype.setMarker = function(pos) {
+    var self = this;
+
+    if (!pos) {
+      var center = this._map.getCenter();
+      pos = {
+        'latitude': center.lat(),
+        'longitude': center.lng()
+      };
+    }
+
+    var latLng = new google.maps.LatLng(pos.latitude, pos.longitude);
+
+    if (!this._marker) {
+      this._marker = new google.maps.Marker({
+        position: latLng,
+        map: this._map,
+        draggable: true
+      });
+      google.maps.event.addListener(this._marker, 'dragend', function() {
+        self.updateData();
+      });
+    } else {
+      this._marker.setPosition(latLng);
+    }
+
+    this._forgetButton.nQuireDynamicMeasureLink('enable');
+  };
+
+  GoogleMapMeasureManager.prototype.forgetMarker = function() {
+    if (this._marker) {
+      this._marker.setMap(null);
+      this._marker = null;
+    }
+  };
+
+  GoogleMapMeasureManager.prototype.updateData = function() {
+    if (this._marker) {
+      var self = this;
+
+      var markerPos = this._marker.getPosition();
+      var positionalRequest = {
+        'locations': [markerPos]
+      };
+
+      var pos = {
+        'latitude': markerPos.lat(),
+        'longitude': markerPos.lng(),
+        'zoom': this._map.getZoom()
+      };
+
+
+      this._serviceDelegate.startRandomDelayProcess();
+      this._elevator.getElevationForLocations(positionalRequest, function(results, status) {
+
+        if (status === google.maps.ElevationStatus.OK && results.length === 1) {
+          pos.altitude = results[0].elevation;
+        }
+
+        var value = JSON.stringify(pos);
+        self._serviceDelegate.saveData(value);
+        self._serviceDelegate.stopRandomDelayProcess();
+      });
+    } else {
+      this._serviceDelegate.saveData('');
+    }
   };
 
 
@@ -34,9 +168,9 @@ $(function() {
 
   nQuireJsSupport.register('GoogleMapMeasure', {
     init: function(dependencies) {
-      $('div[measureType="measure_widget__googlemap"]').each(function() {
+      $('div[measure_type="measure_widget__googlemap"]').each(function() {
         var manager = new GoogleMapMeasureManager(this);
-        var inputElementId = $(this).attr('input-element-id');
+        var inputElementId = $(this).attr('input_element_id');
         dependencies.DynamicMeasureService.registerMeasure(inputElementId, manager);
       });
     }
