0c3c9f92689e0c1c13a25cbc5410d3b7b3222b07
evilfer <evilfer@gmail.com>
2013-02-22 17:24:57 +0000

    - Editgrid integration partially working.

--- modules/custom/pi/activities/pi_data_spreadsheet/js/pi_data_spreadsheet_grid.js
+++ modules/custom/pi/activities/pi_data_spreadsheet/js/pi_data_spreadsheet_grid.js
@@ -1,47 +1,14 @@
 
 $(function() {
   nQuireJsSupport.register('PiDataSpreadsheet', {
-    init: function() {
-    },
-    
-  });
+    init: function(dependencies) {
+      var grid = new editgrid.Grid({
+        sessionKey: dependencies.PiDataSpreadsheetSessionKey.sessionKey,
+        suppressSessionKeyWarning: 1}
+      );
+      var layout = grid.getStandardLayout();
+      layout.doLayout(document.getElementById('pi_data_spreadsheet_grid_container'), grid);
+      grid.openBook({path: 'user/evilfer/' + dependencies.PiDataSpreadsheetSessionKey.bookId});
+    }
+  }, ['PiDataSpreadsheetSessionKey']);
 });
-
-/*
-(function($) {
-  var methods = {
-    init: function(options) {
-      this.addClass('dynamic_measure_link');
-      if (options.disabled) {
-        this.addClass('dynamic_measure_link_disabled');
-      }
-
-      this.click(function() {
-        if (!$(this).hasClass('dynamic_measure_link_disabled')) {
-          options.callback();
-        }
-        return false;
-      });
-      return this;
-    },
-    enable: function() {
-      this.removeClass('dynamic_measure_link_disabled');
-    },
-    disable: function() {
-      this.addClass('dynamic_measure_link_disabled');
-    }
-  };
-
-  $.fn.nQuireDynamicMeasureLink = function(method) {
-    if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    } else if (typeof method === 'object' || !method) {
-      return methods.init.apply(this, arguments);
-    } else {
-      console.log('Method ' + method + ' does not exist on jQuery.nQuireDynamicMeasureLink');
-      return false;
-    }
-  };
-})(jQuery);
-
-*/
