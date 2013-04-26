fc5b9733bbe9fea79ec4d330d26489fe8c4d1745
Eloy Villasclaras <evilfer@gmail.com>
2013-04-26 12:52:39 +0100

    - Spreadsheet readonly mode working, though through ugly workaround. - Key answers cannot be reordered.

--- modules/custom/pi/activities/pi_data_spreadsheet/js/pi_data_spreadsheet_grid.js
+++ modules/custom/pi/activities/pi_data_spreadsheet/js/pi_data_spreadsheet_grid.js
@@ -1,14 +1,26 @@
 
 $(function() {
-  nQuireJsSupport.register('PiDataSpreadsheet', {
-    init: function(dependencies) {
-      var grid = new editgrid.Grid({
-        sessionKey: dependencies.PiDataSpreadsheetSessionKey.sessionKey,
-        suppressSessionKeyWarning: 1}
-      );
-      var layout = grid.getStandardLayout();
-      layout.doLayout(document.getElementById('pi_data_spreadsheet_grid_container'), grid);
-      grid.openBook({path: 'user/evilfer/' + dependencies.PiDataSpreadsheetSessionKey.bookId});
-    }
-  }, ['PiDataSpreadsheetSessionKey']);
+	nQuireJsSupport.register('PiDataSpreadsheet', {
+		loadGrid: function(data) {
+			var grid = new editgrid.Grid({
+				sessionKey: data.sessionKey,
+				suppressSessionKeyWarning: 1
+			}
+			);
+			var layout = grid.getStandardLayout();
+			layout.doLayout(document.getElementById('pi_data_spreadsheet_grid_container'), grid);
+			grid.openBook({path: 'user/evilfer/' + data.bookId});
+		},
+		init: function(dependencies) {
+			var self = this;
+			if (dependencies.PiDataSpreadsheetSessionKey.readonly) {
+				var iframe = $('<iframe style="display:none;">').appendTo('body').attr('onload', function() {
+					self.loadGrid(dependencies.PiDataSpreadsheetSessionKey);
+				}).attr('src', 'http://www.editgrid.com/site/logout');
+
+			} else {
+				this.loadGrid(dependencies.PiDataSpreadsheetSessionKey);
+			}
+		}
+	}, ['PiDataSpreadsheetSessionKey']);
 });
