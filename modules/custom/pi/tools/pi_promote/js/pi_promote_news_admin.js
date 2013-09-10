2f7281ade3697043897027755967f45878ef1d1b
Eloy Villasclaras <evilfer@gmail.com>
2013-09-10 17:10:40 +0100

    Updated forums. Fixed a bug on activity feed: Forum posts marked as still spam appeared on the list.

--- modules/custom/pi/tools/pi_promote/js/pi_promote_news_admin.js
+++ modules/custom/pi/tools/pi_promote/js/pi_promote_news_admin.js
@@ -1,7 +1,36 @@
 
 $(function() {
-  
-  $('news_item_selector').change(function() {
-    
-  });
+
+  function update($fixedElement) {
+    var fixedId = $fixedElement ? $fixedElement.attr('id') : '';
+
+    var fixedValue = $fixedElement ? $fixedElement.val() : 0;
+
+    var items = [];
+
+    $('.news_item_selector select[id!="' + fixedId + '"]').each(function() {
+      var $i = $(this);
+      if ($i.val() > 0) {
+        items.push({id: $(this).attr('id'), weight: parseInt($(this).val())});
+      }
+    });
+
+    items.sort(function(a, b) {
+      return a.weight - b.weight;
+    });
+
+    var value = 1;
+    for (var i in items) {
+      if (value == fixedValue) {
+        value++;
+      }
+
+      $('#' + items[i].id).val(value++);
+    }
+  }
+
+  $('.news_item_selector select').change(function() {
+    update($(this));
+  }
+  );
 });
